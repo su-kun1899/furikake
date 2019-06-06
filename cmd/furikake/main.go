64d319b93bf64106c3d49aa8ce2148b1b05b8d3d
su-kun1899 <higedrum.coz@gmail.com>
2019-06-06 17:04:00 +0900

    add cli sample

--- cmd/furikake/main.go
+++ cmd/furikake/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"github.com/su-kun1899/furikake"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+var (
+	headerOpt  = flag.String("h", "", "header values separated by comma")
+)
 
 func main() {
-	fmt.Printf("hello, world\n")
+	flag.Parse()
+	header := strings.Split(*headerOpt, ",")
+	fName := flag.Args()[0]
+
+	// JSONファイル読み込み
+	bytes, err := ioutil.ReadFile(fName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	csv, err := furikake.ToCsv(header, string(bytes))
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	_, _ = fmt.Println(csv)
 }
